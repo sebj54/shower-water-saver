1f0b6dc83da1804275f42d6d7e263971b95e153f
Sébastien JEAN <sebastien@medialta.com>
2017-01-31 23:50:44 +0100

    Counter - Rename average to objective & add -is-over class

--- js/counter.js
+++ js/counter.js
@@ -4,13 +4,32 @@ Vue.component('shower-counter', app.resolveTemplate('counter', {
         return {
             time: {
                 actual: 0,
-                average: 492
+                objective: 492
             },
             consumption: {
                 actual: 0,
-                average: 65.1
+                objective: 65.1
             },
-            flowRate: 0.13167
+            flowRate: 0.13167,
+            actualProgress: 0
+        }
+    },
+    computed: {
+        actualTime: function()
+        {
+            return this.formatTime(this.time.actual)
+        },
+        objectiveTime: function()
+        {
+            return this.formatTime(this.time.objective)
+        },
+        actualConsumption: function()
+        {
+            return this.formatConsumption(this.consumption.actual)
+        },
+        objectiveConsumption: function()
+        {
+            return this.formatConsumption(this.consumption.objective)
         }
     },
     methods: {
@@ -37,24 +56,6 @@ Vue.component('shower-counter', app.resolveTemplate('counter', {
         formatConsumption: function(liters)
         {
             return liters.toFixed(2) + ' L'
-        }
-    },
-    computed: {
-        actualTime: function()
-        {
-            return this.formatTime(this.time.actual)
-        },
-        averageTime: function()
-        {
-            return this.formatTime(this.time.average)
-        },
-        actualConsumption: function()
-        {
-            return this.formatConsumption(this.consumption.actual)
-        },
-        averageConsumption: function()
-        {
-            return this.formatConsumption(this.consumption.average)
         }
     }
 }))
